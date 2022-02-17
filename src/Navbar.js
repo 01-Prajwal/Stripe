87798ee3fe0f002244859a4decc8d3c622b7c8f5
Prajwal <Prajwalbhange001@gmail.com>
2022-02-17 22:21:26 +0530

    Stripe Menu

---
+++ src/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import logo from './images/logo.svg'
+import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './context'
+
+
+
+const Navbar = () => {
+  const {openSidebar,openSubmenu}=useGlobalContext();
+
+  const displaySubmenu = (e)=>{
+
+
+    const page = e.target.textContent;
+    const tempbtn = e.target.getBoundingClientRect();
+    const center = (tempbtn.left + tempbtn.right)/2;
+    const bottom  = tempbtn.bottom-3;
+    openSubmenu(page,{center,bottom});
+    
+  }
+
+  return <nav className='nav'>
+    <div className="nav-center">
+      <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars/>
+          </button>
+      </div>  
+      <ul className="nav-links">
+        <li>
+          <button className="link-btn"  onMouseOver={displaySubmenu}>products</button>
+        </li>
+        <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>developers</button>
+        </li>
+        <li>
+              <button className="link-btn" onMouseOver={displaySubmenu}>company</button>
+        </li>
+      </ul>
+      <button className="btn signin-btn" >Sign In</button>    </div>
+  </nav>
+}
+
+export default Navbar
